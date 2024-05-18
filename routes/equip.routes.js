0f8f125287e4a7e814674556b847c3f538bf265b
Meryem-megdiche <147342991+Meryem-megdiche@users.noreply.github.com>
2024-05-18 13:35:03 +0100

    5

--- routes/equip.routes.js
+++ routes/equip.routes.js
@@ -14,6 +14,28 @@ router.put("/equip/:id", equipCtrl.apiUpdateequip);
 router.delete('/:id', equipCtrl.apiDeleteequip);
 router.get("/find/:rfid", equipCtrl.apiGetEquipByRfid);
 
-
-
+router.get("/equips", async (req, res) => {
+  try {
+    console.log('Fetching all equipments from database...');
+    const equips = await Equip.find();
+    console.log('Equipments fetched:', equips);
+    res.json(equips);
+  } catch (error) {
+    console.error('Error fetching equipments:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+router.put("/equips/:id", async (req, res) => {
+  try {
+    console.log(`Updating equipment with ID: ${req.params.id}`);
+    const updatedEquip = await Equip.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (updatedEquip) {
+      console.log('Updated equipment:', updatedEquip);
+    }
+    res.json(updatedEquip);
+  } catch (error) {
+    console.error('Error updating equipment:', error);
+    res.status(500).json({ error: error.message });
+  }
+});
 module.exports =  router;
